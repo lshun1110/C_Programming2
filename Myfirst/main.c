44ce8ab53e5531f7cea54c99b5041b60cf25e6a7
user <user@DESKTOP-135026F>
2025-09-03 11:33:47 +0900

    초기 커밋.

---
+++ Myfirst/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void test1();
+
+int main()
+{
+	Profile();
+	return 0;
+}
+
+void test1()
+{
+	int age = 23;
+	
+	const double PI = 3.141592;
+	
+	char myname[128] = "ÀÌ½ÂÈÆ";
+	
+	int i_arr[10] = { 1,2,3,4 };
+}
